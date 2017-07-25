b5fe8da3618bb003e6b03a31e9bb80e4e08890f4
Toni Barth <software@satoprogs.de>
2017-07-25 15:31:49 +0200

    prevent mushclient from stored all last save dates inside the world file

--- lua/yugioh/main.lua
+++ lua/yugioh/main.lua
@@ -11,6 +11,9 @@ TriggerHandler = require('yugioh.triggerhandler')()
 
 function OnWorldOpen()
 
+  -- defining some world-wide options
+  world.SetOption ("omit_date_from_save_files", 1)
+
   -- loading and parsing the configuration file
   local ctbl = Ini.read('config.dat')
 
