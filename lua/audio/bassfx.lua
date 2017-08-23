a881dfcf3d4f13573da35925f24c48f1a9e741e5
Toni Barth <toni.barth@gisa.de>
2017-08-23 11:22:54 +0200

    added version check to bass_fx

--- lua/audio/bassfx.lua
+++ lua/audio/bassfx.lua
@@ -1,10 +1,16 @@
+bit = require("bit")
 class = require("pl.class")
 
 class.BASSFX()
 
 function BASSFX:_init()
 
+  self.bass = require("audio.bindings.bass")
   self.bassfx = require("audio.bindings.bassfx")
+
+  if bit.rshift(self.bass.BASS_GetVersion(), 16) ~= bit.rshift(self:GetVersion(), 16) then
+    error("bass_fx.dll version is not compatible to current bass.dll version.")
+  end
 
 end
 
